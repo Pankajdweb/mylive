64bcc9957f8b767d3d4f3f94a2a64ef92d979138
Ui dev <uidevux@gmail.com>
2021-04-24 22:44:28 +0530

    config file

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'mysite' );
+define( 'DB_NAME', 'pankajkumar' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'ankur_063' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'ankur@063' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
