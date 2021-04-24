f1c3ae990537d621758caffec824fa94122fd9e0
Ui dev <uidevux@gmail.com>
2021-04-24 18:14:59 +0530

    check commit

--- wp-content/themes/mytheme/header-secondary.php
+++ wp-content/themes/mytheme/header-secondary.php
@@ -8,36 +8,33 @@
 
 </head>
 <body  <?php body_class();?>>
-<header class="fixed-top home-header">
+<header class="fixed-top my-header light">
 <div class="container">
-<div class="logo">
-<img src="http://pankajkumar.me/wp-content/uploads/2020/07/my-w-logo.png" alt="my logo" /></a>
-<!-- http://pankajkumar.me/wp-content/uploads/2020/07/my-w-logo.png -->
-<!-- <h3>PANKAJ KUMAR </h3> -->
-</div>
-<div class="menu-home">
-	 <ul id="menu">
-	<li  ><a href="#firstPage">Home</a></li>
-	<li ><a href="#secondPage">About Me</a></li>
-	<li ><a href="#thirdPage">Skills</a></li>
-</ul>
 
-<!-- <nav class="navbar navbar-expand-lg navbar-light ">
+<nav class="navbar navbar-expand-lg navbar-light ">
 <a class="navbar-brand" href="#">Pankaj</a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
 </button>
- <div>
-	 <ul id="menu">
-	<li data-menuanchor="firstPage" class="active"><a href="#firstPage">First section</a></li>
-	<li data-menuanchor="secondPage"><a href="#secondPage">Second section</a></li>
-	<li data-menuanchor="thirdPage"><a href="#thirdPage">Third section</a></li>
-	<li data-menuanchor="forthPage"><a href="#forthPage">Fourth section</a></li>
-</ul>
-</div>
-</nav> -->
 
+<?php 
+wp_nav_menu (
+  array (
+    'theme_location'  =>  'top-menu', 
+    'container'       => 'div',
+    'container_class' => 'collapse navbar-collapse justify-content-center',
+    'container_id'    => 'navbarSupportedContent',
+    'item_spacing'    => 'preserve',
+    'menu_class' => 'navbar-nav m-auto'
+   
+  )
+);
+?>
 
+ 
+</nav>
 
 </div>
 </header>
+
+
